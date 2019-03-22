bd9d815cb7e8c33ef7a42918a1442e3d8edc8f5f
hasetennisth <hasetennis52@gmail.com>
2019-03-22 23:29:15 +0900

    Update

---
+++ src/components/character.vue
@@ -0,0 +1,21 @@
+<template>
+<div id='player' class='border d-inline'>A</div>
+</template>
+
+<script>
+export default {
+    name: 'character',
+
+}
+</script>
+
+<style scoped>
+#player {
+    position: fixed;
+    width:30px;
+    height:30px;
+    left: 50%;
+    z-index: 1;
+    background:red;
+}
+</style>
--- src/components/main.vue
+++ src/components/main.vue
@@ -2,10 +2,16 @@
 <div class='main'>
     <div id='scene'>
         <div class='ground'>
+
             <div class='ground_row row' v-for='n in 14' :key='n'>
-              <div class='ground_child col-1'  v-for='n in 12' :key='n'></div>
-              </div>
-           
+                <div class='ground_child col-1' v-for='m in 12' :key='m'>
+                    <!-- 中央にキャラクター配置(row:7,col:7) -->
+                    <div v-if='n==7 && m ==7'>
+                        <Chara />
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 </div>
@@ -14,24 +20,26 @@
 <script>
 import * as mainjs from '../assets/js/index.js';
 import * as stagejs from '../assets/js/stage.js';
+import Chara from './character.vue';
 
-console.log(stagejs.read('ground'))
 export default {
+    name: 'game',
     data() {
         return {
 
         }
     },
+    components: {
+        Chara
+    },
     mounted() {
-        
+
     },
     methods: {
 
     }
 
 }
-
-
 </script>
 
 <style scoped>
@@ -45,24 +53,26 @@ export default {
     width: 360px;
     height: inherit;
     margin: 0 auto;
-    
+
 }
+
 .ground {
     /* width: 30px;
     height: 30px;
     position: relative;
     overflow: hidden; */
 }
-.ground_row{
+
+.ground_row {
     margin: 0px !important;
 }
-.ground_child{
+
+.ground_child {
     background: url('../assets/img/field/field1.png') no-repeat center center;
     object-fit: cover;
-    width:30px;
-    height:30px;
-    margin:0 !important;
+    width: 30px;
+    height: 30px;
+    margin: 0 !important;
 
 }
-
 </style>
