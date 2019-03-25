351a060156a917036c60b5dcf09416f8b9f052ae
hasetennisth <hasetennis52@gmail.com>
2019-03-25 12:32:03 +0900

    Update

--- src/components/controller.vue
+++ src/components/controller.vue
@@ -1,12 +1,20 @@
 <template>
 <div id='controllers'>
     <div class="row">
-        <div id='control_top' class='col-6 control_btn border' @click='top_click()'><div class='control_kn'><i class="fas fa-arrow-up"></i></div></div>
-        <div id='control_right' class='col-6 control_btn border' @click='right_click()'><div class='control_kn'><i class="fas fa-arrow-right"></i></div></div>
+        <div id='control_top' class='col-6 control_btn border' @click='top_click()'  @mousedown="sample()" @mouseup='release_btn()'>
+            <div class='control_kn'><i class="fas fa-arrow-up"></i></div>
+        </div>
+        <div id='control_right' class='col-6 control_btn border' @click='right_click()'>
+            <div class='control_kn'><i class="fas fa-arrow-right"></i></div>
+        </div>
     </div>
     <div class="row">
-        <div id='control_left' class='col-6 control_btn border' @click='left_click()'><div class='control_kn'><i class="fas fa-arrow-left"></i></div></div>
-        <div id='control_bottom' class='col-6 control_btn border' @click='bottom_click()'><div class='control_kn'><i class="fas fa-arrow-down"></i></div></div>
+        <div id='control_left' class='col-6 control_btn border' @click='left_click()'>
+            <div class='control_kn'><i class="fas fa-arrow-left"></i></div>
+        </div>
+        <div id='control_bottom' class='col-6 control_btn border' @click='bottom_click()'>
+            <div class='control_kn'><i class="fas fa-arrow-down"></i></div>
+        </div>
     </div>
 </div>
 </template>
@@ -16,18 +24,51 @@ import * as stagejs from '../assets/js/stage.js';
 
 export default {
     name: 'control',
+    data() {
+        return {
+            stop_count: 1000,
+            nowcount: 0,
+            now_put:false
+        }
+    },
     methods: {
         top_click() {
-            this.$emit('stage_reload',0,-1,'top');
+            this.$emit('stage_reload', 0, -1, 'top');
         },
         left_click() {
-            this.$emit('stage_reload',-1,0,'left');
+            this.$emit('stage_reload', -1, 0, 'left');
         },
         right_click() {
-            this.$emit('stage_reload',1,0,'right');
+            this.$emit('stage_reload', 1, 0, 'right');
         },
         bottom_click() {
-            this.$emit('stage_reload',0,1,'bottom');
+            this.$emit('stage_reload', 0, 1, 'bottom');
+        },
+        sample() {
+            
+            let this_ = this;
+            var count = 0;     
+            var countup = function () { 
+                count++;
+                this_.nowcount = count;
+                var id = setTimeout(countup, 100);
+                if (count > this_.stop_count ) {
+                    clearTimeout(id); //idをclearTimeoutで指定している
+                    this_.stop_count  =1000;
+                    this_.nowcount = 0;
+                }
+                if(count > 3){ this_.now_put = true;}
+                if(this_.now_put == true){ 
+                this_.top_click();
+                }
+            }
+            countup();
+            
+        },
+        release_btn() {
+            this.now_put = false;
+            console.log(this.nowcount)
+            this.stop_count = this.nowcount;
         }
     }
 }
@@ -40,45 +81,53 @@ export default {
 
 #controllers {
     text-align: center;
-    width: 120px;
-    height: 120px;
+    width: 100px;
+    height: 100px;
     margin: 0 auto;
     /* clip-path: polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%); */
     transform: rotate(45deg);
 }
 
 #control_top {
-    width: 60px;
-    height: 60px;
+    background: gray;
+    width: 50px;
+    height: 50px;
 }
 
 #control_right {
-    width: 60px;
-    height: 60px;
+    background: gray;
+    width: 50px;
+    height: 50px;
 }
 
 #control_left {
-    width: 60px;
-    height: 60px;
+    background: gray;
+    width: 50px;
+    height: 50px;
 }
 
 #control_bottom {
-    width: 60px;
-    height:60px;
+    background: gray;
+    width: 50px;
+    height: 50px;
 }
-.control_btn{
+
+.control_btn {
     border-radius: 50%;
 }
-.control_kn{
-   color:white; 
-   transform:rotate(-45deg);
-   margin:0 auto;
-   top:10;
-   right:0;
-   left:0;
-   bottom:0;
-   padding-top:5px;
-   padding-right:5px;
-   font-size:30px;
+
+.control_kn {
+    color: white;
+
+    transform: rotate(-45deg);
+    margin: 0 auto;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    padding-top: 0px;
+    padding-right: 30px;
+    padding-left: 0px;
+    font-size: 25px;
 }
 </style>
